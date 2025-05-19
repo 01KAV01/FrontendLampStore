490a6658395f14198254725c569fc57c2c173612
01KAV01 <ladno0001@gmail.com>
2025-05-19 16:04:25 +0400

    init

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   const { wishlist, setWishlist, wishQuantity } = useWishlistStore();
 
 const handleLogout = () => {
-    setTimeout(() => signOut({ callbackUrl: "https://frontendlampstore-production.up.railway.app/login" }), 500); // или другой нужный путь
+    setTimeout(() => signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_API_URL}`}), 500); // или другой нужный путь
     toast.success("Выход из аккаунта выполнен успешно!");
   };
 
