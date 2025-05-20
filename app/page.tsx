262d51918033c33f84ac609d3b28209c444cadb8
01KAV01 <ladno0001@gmail.com>
2025-05-20 08:09:49 +0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { CategoryMenu, Hero, Incentives, IntroducingSection, Newsletter, ProductsSection } from "@/components";
+import {  Hero, Incentives, IntroducingSection, Newsletter, ProductsSection } from "@/components";
 
 export default function Home() {
   return (
     <>
     <Hero />
     <IntroducingSection />
-    <CategoryMenu />
+
     <ProductsSection />
     </>
   );
