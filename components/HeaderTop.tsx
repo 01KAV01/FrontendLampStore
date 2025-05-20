8202e5486b62ebb7aa48448ca569bf67223ea747
Artem <ladno0001@gmail.com>
2025-05-20 05:09:12 +0400

    init

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -14,7 +14,7 @@ const HeaderTop = () => {
   const { data: session }: any = useSession();
 
 const handleLogout = () => {
-    setTimeout(() => signOut({ callbackUrl: "/login", redirect: true }), 500);
+    setTimeout(() => signOut(), 500);
     toast.success("Выход из аккаунта выполнен успешно!");
 };
   return (
