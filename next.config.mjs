f81cb7786f243f160938e0d7e2c0ba2a1a12795d
01KAV01 <ladno0001@gmail.com>
2025-05-20 06:17:20 +0400

    init

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,38 @@ const nextConfig = {
         ],
         
       },
+callbacks: {
+    async session({ session, token }) {
+      //logic...
+      return session;
+    },
+    async jwt({ token, user, accessToken }) {
+      //logic...
+      return token;
+    },
+    authorized({ request, auth }) {
+      // auth logic...
+    },
+    async redirect({ url, baseUrl }) {
 
+      const frontendUrl = process.env.NEXTAUTH_URL || baseUrl;
+
+      if (url.startsWith('/')) {
+        return `${frontendUrl}${url}`;
+      }
+
+      try {
+        const parsedUrl = new URL(url);
+        if (parsedUrl.origin === frontendUrl) {
+          return url;
+        }
+      } catch (error) {
+        console.error('Invalid URL in redirect:', url);
+      }
+
+      return baseUrl;
+    },
+  },
 }
 
 
