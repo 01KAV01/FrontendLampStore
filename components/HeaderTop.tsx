74cbac711e726c1f58f11f2c93ed5f9c0f7d3ee1
01KAV01 <ladno0001@gmail.com>
2025-05-20 05:13:45 +0400

    init

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -14,7 +14,7 @@ const HeaderTop = () => {
   const { data: session }: any = useSession();
 
 const handleLogout = () => {
-    setTimeout(() => signOut(), 500);
+    setTimeout(() => signOut({ callbackUrl: "/login", redirect: true }), 500);
     toast.success("Выход из аккаунта выполнен успешно!");
 };
   return (
