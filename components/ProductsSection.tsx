2855d351e5e9d642aa852caef7f23444b6924812
Artem <ladno0001@gmail.com>
2025-06-08 00:34:27 +0400

    init

--- components/ProductsSection.tsx
+++ components/ProductsSection.tsx
@@ -1,60 +1,26 @@
-
-
 import React from "react";
 import ProductItem from "./ProductItem";
 import Heading from "./Heading";
 
-
-
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import {
-  Breadcrumb,
-  Filters,
-  Pagination,
-  Products,
-  SortBy,
-} from "@/components";
-
-// improve readabillity of category text, for example category text "smart-watches" will be "smart watches"
-const improveCategoryText = (text: string): string => {
-  if (text.indexOf("-") !== -1) {
-    let textArray = text.split("-");
-
-    return textArray.join(" ");
-  } else {
-    return text;
-  }
-};
-
-
-const ShopPage = (slug: any) => {
-  
+const ProductsSection = async () => {
+  // sending API request for getting all products
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+  const products = await data.json();
   return (
-    <div className="text-black bg-white">
-      <div className=" max-w-screen-2xl mx-auto px-10 max-sm:px-5">
-        <Breadcrumb />
-        <div className="grid grid-cols-[200px_1fr] gap-x-10 max-md:grid-cols-1 max-md:gap-y-5">
-          <Filters />
-          <div>
-            <div className="flex justify-between items-center max-lg:flex-col max-lg:gap-y-5">
-              <h2 className="text-2xl font-bold max-sm:text-xl max-[400px]:text-lg uppercase">
-                {slug?.params?.slug && slug?.params?.slug[0]?.length > 0
-                  ? improveCategoryText(slug?.params?.slug[0])
-                  : "Вся продукция"}
-              </h2>
-
-              <SortBy />
-            </div>
-            <div className="divider"></div>
-            <Products slug={slug} />
-            <Pagination />
-          </div>
+    <div className="bg-blue-500 border-t-4 border-white">
+      <div className="max-w-screen-2xl mx-auto pt-20">
+        <Heading title="ПРЕДЛАГАЕМЫЕ ТОВАРЫ" />
+        <div className="grid grid-cols-4 justify-items-center max-w-screen-2xl mx-auto py-10 gap-x-2 px-10 gap-y-8 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
+          {products.map((product: Product) => (
+            <ProductItem key={product.id} product={product} color="white" />
+          ))}
         </div>
       </div>
     </div>
   );
 };
 
-export default ShopPage;
+export default ProductsSection;
