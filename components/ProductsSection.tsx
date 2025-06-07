e153c8ae113ee1f44fcc0bbf13cae369481c3ac7
01KAV01 <ladno0001@gmail.com>
2025-06-08 02:20:57 +0400

    init

--- components/ProductsSection.tsx
+++ components/ProductsSection.tsx
@@ -14,7 +14,15 @@ import {
 } from "@/components";
 
 
+const improveCategoryText = (text: string): string => {
+  if (text.indexOf("-") !== -1) {
+    let textArray = text.split("-");
 
+    return textArray.join(" ");
+  } else {
+    return text;
+  }
+};
 
 const ProductsSection = (slug: any) => {
   // sending API request for getting all products
@@ -25,12 +33,29 @@ const ProductsSection = (slug: any) => {
         <Heading title="ЛУЧШЕЕ ЗА ПОСЛЕДНЕЕ ВРЕМЯ" />
         <div className="divider"></div>
         <Products slug={slug} />
+                      <h2 className="text-2xl font-bold max-sm:text-xl max-[400px]:text-lg uppercase">
+                {slug?.params?.slug && slug?.params?.slug[0]?.length > 0
+                  ? improveCategoryText(slug?.params?.slug[0])
+                  : "Вся продукция"}
+              </h2>
         <div className="grid grid-cols-4 justify-items-center max-w-screen-2xl mx-auto py-10 gap-x-2 px-10 gap-y-8 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
-          <Products slug={slug} />
+                        <h2 className="text-2xl font-bold max-sm:text-xl max-[400px]:text-lg uppercase">
+                {slug?.params?.slug && slug?.params?.slug[0]?.length > 0
+                  ? improveCategoryText(slug?.params?.slug[0])
+                  : "Вся продукция"}
+              </h2>
         </div>
-        <Products slug={slug} />
+                      <h2 className="text-2xl font-bold max-sm:text-xl max-[400px]:text-lg uppercase">
+                {slug?.params?.slug && slug?.params?.slug[0]?.length > 0
+                  ? improveCategoryText(slug?.params?.slug[0])
+                  : "Вся продукция"}
+              </h2>
       </div>
-              <Products slug={slug} />
+                    <h2 className="text-2xl font-bold max-sm:text-xl max-[400px]:text-lg uppercase">
+                {slug?.params?.slug && slug?.params?.slug[0]?.length > 0
+                  ? improveCategoryText(slug?.params?.slug[0])
+                  : "Вся продукция"}
+              </h2>
     </div>
   );
 };
