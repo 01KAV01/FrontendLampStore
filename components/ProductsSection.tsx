2c99f5a0d69db02427ff262ee8388090349d4207
01KAV01 <ladno0001@gmail.com>
2025-06-08 01:35:17 +0400

    init

--- components/ProductsSection.tsx
+++ components/ProductsSection.tsx
@@ -5,18 +5,34 @@ import Heading from "./Heading";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-const ProductsSection = async () => {
+import {
+  Products
+} from "@/components";
+
+const improveCategoryText = (text: string): string => {
+  if (text.indexOf("-") !== -1) {
+    let textArray = text.split("-");
+
+    return textArray.join(" ");
+  } else {
+    return text;
+  }
+};
+
+const ProductsSection = (slug: any) => {
   // sending API request for getting all products
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
-  const products = await data.json();
+
   return (
     <div className="bg-blue-500 border-t-4 border-white">
       <div className="max-w-screen-2xl mx-auto pt-20">
-        <Heading title="ПРЕДЛАГАЕМЫЕ ТОВАРЫ" />
+        <Heading title="ЛУЧШЕЕ ЗА ПОСЛЕДНЕЕ ВРЕМЯ" />
         <div className="grid grid-cols-4 justify-items-center max-w-screen-2xl mx-auto py-10 gap-x-2 px-10 gap-y-8 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
-          {products.map((product: Product) => (
-            <ProductItem key={product.id} product={product} color="white" />
-          ))}
+                {slug?.params?.slug && slug?.params?.slug[0]?.length > 0
+                  ? improveCategoryText(slug?.params?.slug[0])
+                  : ""}
+
+          
+                      <Products slug={slug} />
         </div>
       </div>
     </div>
